79574f1cae86a74fcd9b78eebe49e9ce193336cd
excelaiyt <excelaiyt12@gmail.com>
2023-10-22 17:17:57 +0300

    add commit

---
+++ src/CharAddition.java
@@ -0,0 +1,13 @@
+public class CharAddition {
+    public static void main(String[] args) {
+       /* CharExpression expression = new CharExpression();
+       int result = expression.charExpression(15, 20, 100, 12, 13, 14, 15);
+        System.out.println(result);*/
+        int result = charExpression(15);
+        System.out.println(result);
+    }
+
+    public static char charExpression(int a) {
+        return (char) ('q' + a);
+    }
+}
