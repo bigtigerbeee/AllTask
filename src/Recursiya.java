6dae352c36f0855ca67521126d9db440d19d14c1
excelaiyt <excelaiyt12@gmail.com>
2023-10-23 17:20:48 +0300

    add commit

---
+++ src/Recursiya.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+
+public class Recursiya {
+    public static void main(String[] args) {
+        System.out.println(factorial(6));
+    }
+
+    public static BigInteger factorial(int value) {
+        if (value <= 1) {
+            return BigInteger.valueOf(1);
+        } else {
+            return BigInteger.valueOf(value * (value - 1));
+
+        }
+    }
+}
