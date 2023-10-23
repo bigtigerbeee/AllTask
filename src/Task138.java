c4a65f808b6a58db166780402416a5c1fe3bb72c
excelaiyt <excelaiyt12@gmail.com>
2023-10-23 16:39:10 +0300

    add commit

--- src/Task138.java
+++ src/Task138.java
@@ -1,12 +1,13 @@
 public class Task138 {
+
     public static void main(String[] args) {
         isPowerOfTwo2(10);
         System.out.println(isPowerOfTwo2(10));
     }
 
-        public static boolean isPowerOfTwo2(int a) {
+    public static boolean isPowerOfTwo2(int a) {
 
-            return (a > 0) && (Integer.bitCount(a) == 1);
-        }
+        return (a > 0) && (Integer.bitCount(a) == 1);
     }
+}
 
